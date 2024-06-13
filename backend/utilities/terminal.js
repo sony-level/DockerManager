d595c386930c5a657af6c8a13349bcf52b98a560
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-06-13 02:46:21 +0200

    chore: Update npm dependency to @tensorflow/tfjs-node@4.20.0

--- backend/utilities/terminal.js
+++ backend/utilities/terminal.js
@@ -1,8 +1,16 @@
 const child_process = require("child_process");
+const readline = require("readline");
+const tf = require("@tensorflow/tfjs-node")
 
 const isValidId = (id) => /^[0-9a-zA-Z]+$/.test(id.trim());
 const isValidString = (id) => /^[a-zA-Z]+$/.test(id.trim());
 
+const r1 = readline.createInterface({
+  input: procces.stdin,
+  output: process.stdout
+});
+
+// Fonction pour exécuter une commande dans le terminal
 const Terminal = (command) =>
   new Promise((resolve, reject) => {
     child_process.exec(
@@ -15,33 +23,77 @@ const Terminal = (command) =>
     );
   });
 
+  const addToDockerGroup = async () => {
+    try {
+      const username = await Terminal('whoami');
+      await Terminal(`sudo usermod -aG docker ${username.trim()}`);
+      console.log(`L'utilisateur ${username.trim()} a été ajouté au groupe docker.`);
+      console.log('Vous devez vous déconnecter et vous reconnecter pour que les modifications prennent effet.');
+    } catch (error) {
+      console.error('Une erreur s\'est produite lors de l\'ajout de l\'utilisateur au groupe docker :', error);
+    }
+  };
+  addToDockerGroup();
+
+
+  const checkDocker = async () => {
+    try {
+      await Terminal('docker -v');
+      console.log('Genial,  Docker est installé sur votre machine');
+    } catch (error) {
+      console.log('Docker n\'est pas installé sur votre machine');
+      r1.question('Voulez-vous installer Docker maintenant ? (y/n)', async (answer) => {
+        if (answer.toLowerCase() === 'y') {
+          await Terminal('sudo apt-get update');
+          await Terminal('sudo apt-get install docker.io');
+          await Terminal('sudo systemctl start docker');
+          await Terminal('sudo systemctl enable docker');
+          console.log('Docker est installé avec succès');
+        } else {
+          console.log('Vous devez installer Docker pour continuer');
+          process.exit(1);
+        }
+        r1.close();
+      });
+    }
+  };
+
 exports.safeTerminal = {
+
+  // Installer les modules npm dans le chemin spécifié
   installModules: async (backendPath) => {
     await Terminal(`cd ${backendPath} && npm install`);
   },
+  // Lancer l'application Node.js dans le chemin spécifié
   serve: async (backendPath) => {
     await Terminal(`cd ${backendPath} && node index.js`);
   },
-  allContainers: () => Terminal(`docker ps -q -a`),
+   // Récupérer tous les conteneurs Docker
+  allContainers:  async () => { 
+    await checkDocker();
+    return Terminal(`docker ps -q -a`);
+  },
+  // Récupérer les conteneurs en cours d'exécution
   inspectContainer: async (id) => {
     if (isValidId(id)) {
       return Terminal(`docker container inspect ${id}`);
     } else {
-      throw new Error("The container id is invalid");
+      throw new Error("L'ID du conteneur est invalide");
     }
   },
+   // Exécuter une tâche générique sur un conteneur Docker
   generic: async (task, id) => {
     if (!isValidString(task)) {
-      throw new Error("The task command is invalid.");
+      throw new Error("La commande de tâche est invalide.");
     }
     if (!isValidId(id)) {
-      throw new Error("The container id is invalid");
+      throw new Error("L'ID du conteneur est invalide");
     }
     return Terminal(`docker container ${task} ${id}`);
   },
   logs: async (id) => {
     if (!isValidId(id)) {
-      throw new Error("The container id is invalid");
+      throw new Error("L'ID du conteneur est invalide");
     }
     return Terminal(`docker container logs ${id} --tail 1500`);
   },
@@ -52,7 +104,7 @@ exports.safeTerminal = {
     ),
   prune: (pruneType) => {
     if (!isValidString(pruneType)) {
-      throw new Error("The entity type is not valid");
+      throw new Error("TLe type d'entité n'est pas valide");
     }
     return Terminal(`docker ${pruneType} prune -f`);
   },
@@ -63,10 +115,10 @@ exports.safeTerminal = {
     ),
   singleImage: (task, id) => {
     if (!isValidString(task)) {
-      throw new Error("The task command is invalid.");
+      throw new Error("La commande de tâche est invalide.");
     }
     if (!isValidId(id)) {
-      throw new Error("The image id is invalid");
+      throw new Error("L'ID de l'image est invalide.");
     }
     if (task == "run") {
       return Terminal(`docker ${task} ${id}`);
@@ -75,3 +127,19 @@ exports.safeTerminal = {
     }
   },
 };
+
+// Vérifier si Docker est installé au démarrage du script
+checkDocker().then(() => {
+  rl.close();
+}).catch((error) => {
+  console.error('Une erreur s\'est produite :', error);
+  rl.close();
+});
+
+// Fonction pour recuperer les logs d'un conteneur
+const getDockerLogs = async (id) => {
+if (!/^[0-9a-zA-Z]+$/.test(id.trim())) {
+  throw new Error("L'ID du conteneur est invalide");
+}
+  return await Terminal(`docker container logs ${id} --tail 1500`);
+}

78be3cf2a8d65b9cfd752448dd86b37ff2bdc520
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-06-14 12:50:21 +0200

    chore: Update npm dependencies and add Docker Manager utility functions

--- client/src/index.js
+++ client/src/index.js
@@ -10,13 +10,43 @@ import Loading from './components/loading'
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isInactive, setIsInactive] = useState(false);
 
   const handleStart = () => {
     setIsLoading(false);
+    localStorage.setItem('appLaunched', 'true');
   };
 
+  const resetInactivityTimer = useCallback(() => {
+    setIsInactive(false);
+    clearTimeout(window.inactivityTimer);
+    window.inactivityTimer = setTimeout(() => {
+      setIsInactive(true);
+    }, 3600000); // 1 heure en millisecondes
+  }, []);
+
+  useEffect(() => {
+    const firstLaunch = localStorage.getItem('appLaunched') !== 'true';
+    if (firstLaunch) {
+      setIsLoading(true);
+    } else {
+      setIsLoading(false);
+    }
+
+    window.addEventListener('mousemove', resetInactivityTimer);
+    window.addEventListener('keypress', resetInactivityTimer);
+
+    resetInactivityTimer(); // Initialiser le timer au démarrage
+
+    return () => {
+      window.removeEventListener('mousemove', resetInactivityTimer);
+      window.removeEventListener('keypress', resetInactivityTimer);
+      clearTimeout(window.inactivityTimer);
+    };
+  }, [resetInactivityTimer]);
+
   return (
-    isLoading ? <Loading onStart={handleStart} /> :
+    (isLoading || isInactive) ? <Loading onStart={handleStart} /> :
     <Provider store={store}>
       <Routes />
     </Provider>
